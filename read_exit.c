ed885007eb1be6476ebf985d86646d41c10c5a49
Pranava116 <pranavagrao116@gmail.com>
2026-03-02 20:00:35 +0530

    feat: serialization and deserialization of values to rows and raw mem

--- read_exit.c
+++ read_exit.c
@@ -1,19 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-
+#define COLUMN_USERNAME_LENGTH 32
+#define COLUMN_EMAIL_LENGTH 255
+#define size_of_attribute(Struct, Attribute) sizeof((Struct*)0) -> Attribute)
+//the above line is a macro that gives the size of the attribute in the struct
+const unit32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+//in disk we dont seperate and store the valaues of id, username, email instead we store raw bytes and to know when the next values stat we use offsets
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_OFFSET+USERNAME_OFFSET+EMAIL_OFFSET;
 typedef enum{
   META_COMMAND_SUCCESS,
   META_COMMAND_UNRECOGNIZED
 }MetaCommandResult;
-
+typedef struct{
+  uint32 id;
+  char username[COLUMN_USERNAME_LENGTH];
+  char email[COLUMN_EMAIL_LENGTH];
+}Row;
 typedef enum{
   STATEMENT_INSERT,
   STATEMENT_SELECT
 }StatementType;
 typedef struct{
   StatementType type;
+  Row row_to_insert;
 }Statement;
 typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT} PrepareResult;
 typedef struct{
@@ -30,6 +45,19 @@ InputBuffer* new_input_buffer(){
   return  input_buffer;
 }
 
+
+void serialize_values(Row* source, void* destination){
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination +USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+// so what er do is we take ths tructed data like struct and convert them to raw bytes to store in memory that is why the desitnation type is void 
+//
+void deserialize_values(void* source, Row* destination){
+  memcpy(&(destination->id), source+ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source+USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source+EMAIL_OFFSET, EMAIL_SIZE);
+}
 void printf_prompt(){
   printf("db > ");
 
@@ -60,6 +88,10 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer,Statement* statement ){
   if(strcmp( input_buffer->buffer, "insert") == 0){
     statement->type = STATEMENT_INSERT;
+    int args_assignment = sscnf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),statement->row_to_insert.username, statement->row_to_insert.email);
+    if(args_assignment<3){
+      return PREPARE_SYNTAX_ERROR;
+    }
     return PREPARE_SUCCESS;
   }
   if(strcmp(input_buffer->buffer, "select") == 0){
